cb9e4072434e37a77314df10aec3f45304265fd4
Archit Agarwal <archit.agarwal@kaplan.edu>
2024-06-07 00:11:15 +0530

    first commit

---
+++ GetListOfUsers.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+#############################
+#Author : Archit Agarwal
+#Date: 7th June 24
+#About : This script returns the list of users with read access to a github repository.
+#This script expects command line arguments after the execute command. 
+#Example - ./<scriptname> <gitHub Organization/Username> <Repo Name>
+#############################
+
+
+function helper{
+    checksum=2
+    if [[ $# -ne checksum]];
+    echo "Please Enter the command line arguments to properly run this bash script"
+    echo "Expected args after execute commands are <gitHub Organization/Username> <Repo Name>"
+}
+
+# GitHub API URL
+API_URL="https://api.github.com"
+
+# GitHub username and personal access token
+USERNAME=$username
+TOKEN=$token
+
+# User and Repository information
+REPO_OWNER=$1
+REPO_NAME=$2
+
+# Function to make a GET request to the GitHub API
+function github_api_get {
+    local endpoint="$1"
+    local url="${API_URL}/${endpoint}"
+
+    # Send a GET request to the GitHub API with authentication
+    curl -s -u "${USERNAME}:${TOKEN}" "$url"
+}
+
+# Function to list users with read access to the repository
+function list_users_with_read_access {
+    local endpoint="repos/${REPO_OWNER}/${REPO_NAME}/collaborators"
+
+    # Fetch the list of collaborators on the repository
+    collaborators="$(github_api_get "$endpoint" | jq -r '.[] | select(.permissions.pull == true) | .login')"
+
+    # Display the list of collaborators with read access
+    if [[ -z "$collaborators" ]]; then
+        echo "No users with read access found for ${REPO_OWNER}/${REPO_NAME}."
+    else
+        echo "Users with read access to ${REPO_OWNER}/${REPO_NAME}:"
+        echo "$collaborators"
+    fi
+}
+
+# Main script
+
+echo "Listing users with read access to ${REPO_OWNER}/${REPO_NAME}..."
+list_users_with_read_access
